2e496dc6e9708d1003bab914551ed30f44ee039d
Huguez <carlos.huguez@gmail.com>
2015-09-18 08:45:49 -0700

    se le hicieron modificaciones a refister.php y diversas pruebas a form.php

--- form.php
+++ form.php
@@ -9,25 +9,53 @@
 <html lang="en">
 	<head>
 		<title>Registrarse</title>
+		<!--<style type="text/css">
+			.errorUsuario, .errorPass, .errorCorreo {
+				background: orange;
+				border: solid 2px red;
+			}
+			 .msg { color: green; } 
+		</style>-->
 		<meta charset="UTF-8">
 		<link rel="shortcut icon" type="image/x-icon" href="http://4.bp.blogspot.com/_ymlILn01FV4/TJtgk8wfKmI/AAAAAAAAABg/75GYDggDUHw/s1600/H.jpg">
 	</head>
 	<body>
-		<form method="post" action="register.php">
+		<h3>Crear Nuevo Usuario</h3>
+		<form method="post" action="register.php" >
 			<br><br>
-			<input type="text" maxlength="30" placeholder="Emal..." name="email" autocomplete="off" required/><br><br>
+			<label>Nombre(s): </label>
+			<input type="text" maxlength="30" placeholder="Jose, Pedro, Gonsalo" name="name" autocomplete="off" required/><br><br>
+			<label>Apellido(s): </label>
+			<input type="text" maxlength="30" placeholder="Huguez, lopez Fimbres, Olmos Morales" name="lastName" autocomplete="off" required/><br><br>
 			
-			<input type="text" maxlength="30" placeholder="Username..." name="user" autocomplete="off" required/><br><br>
+			<!--<div class = "<?php echo $GLOBALS['errorCorreo']; ?>" > -->
+			<div clas="">
+				<label>Correo electronico:  </label>
+				<input type="text" maxlength="30" placeholder="persona@email.com" name="email" autocomplete="off" required/>
+				<!-- <span class = "msg"> <?php echo $GLOBALS['msgCorreo']; ?> </span> -->
+				<br><br>
+			</div>
 
-			<input type="password" maxlength="20" placeholder="Password..." name="pass" autocomplete="off" required /><br><br>
+			<!--<div class = "<?php echo $GLOBALS['errorUsuario']; ?>">-->
+			<div class = "">
+				<label>Usuario: </label>
+				<input type="text" maxlength="30" placeholder="individuo15_h" name="user" autocomplete="off" required/>
+				<!-- <span class="msg"><?php echo $GLOBALS['errorUsuario']; ?></span>-->
+				<br><br>
+			</div>
 			
-			<input type="password" maxlength="20" placeholder="Password Again..." name="pass" autocomplete="off" required /><br><br>
+			 <!--<div class="<?php echo $GLOBALS['errorPass']; ?>"> -->
+			 <div class="">
+				<label>Contraseña: </label>
+				<input type="password" maxlength="20" placeholder="Contraseña" name="pass" autocomplete="off" required /><br><br>
+				<label>Confirmar Contraseña: </label>
+				<input type="password" maxlength="20" placeholder="Contraseña otra vez" name="pass2" autocomplete="off" required />
+				<!-- <span class="msg"><?php echo $GLOBALS['errorPass']; ?></span>-->
+				<br><br>
+			</div>
 			
-			<input type="text" maxlength="30" placeholder="Name..." name="name" autocomplete="off" required/><br><br>
+			<button type="subimit" class="btn" name="register" value="Registrarse">Registrarse</button>
 
-			<input type="text" maxlength="30" placeholder="LastName..." name="lastname" autocomplete="off" required/><br><br>
-
-			<button type="subimit" class="btn btn-success" name="register" value="Registrarse">Subir</button>
 		</form>
 	</body>
 </html>
--- register.php
+++ register.php
@@ -1,27 +1,51 @@
 <?php
 	session_start();
 
-	include 'server.php';
-	
-	$email = $_POST['email'];
-	$user = $_POST['user']; 
-	$password = $_POST['password'];
-	$name = $_POST['name'];
+ 	$correo = $_POST['email'];
+ 	$contrasena = $_POST["pass"];
+ 	$contrasena2 = $_POST["pass2"];
+ 	$usuario = $_POST["user"];
+ 	$name = $_POST['name'];
 	$lastname = $_POST['lastName'];
+	$error = False;
 
-	$email
+ 	if( $contrasena2 != $contrasena ){
+ 		$error = True;
+ 		echo '<script type="text/javascript"> alert("las contraseña no coinciden, vuelva a intentarlo.");</script>';
+ 	}
 
-	$result = mysql_query("INSERT INTO USER ( Email, User, Password, Name, lastName )  VALUES ( '$email', '$user', '$password', '$name', '$lastname' )");
-	$num = mysql_affected_rows();
-	
-	if( $num == 1 ){
-		$_SESSION['user'] = $user;
-		header("Location: main.php");
-		exit(); 
-	}elseif ( !$num ) {
+	include 'server.php';
+
+ 	$result = mysql_query("SELECT * FROM USER WHERE User = '$usuario'");
+	$num = mysql_num_rows( $result );
+
+ 	if( $num == 1 ){
+ 		$error = True;
+ 		echo '<script type="text/javascript"> alert("El usuario que usted eligio ya esta siendo ocupado, elija otro por favor.");</script>';
+ 	}
+
+	$result = mysql_query("SELECT * FROM USER WHERE Email = '$correo'");
+	$num = mysql_num_rows( $result );
+
+ 	if( $num == 1 ){
+ 		$error = True;
+ 		echo '<script type="text/javascript"> alert("No se puede tener 2 usuarios con un mismo correo electronico increse otro por favor.");</script>';
+ 	}
+
+ 	if( !$error ){
+ 		$result = mysql_query("INSERT INTO USER ( Email, User, Password, Name, lastName )  VALUES ( '$correo', '$usuario', '$contrasena', '$name', '$lastname' )");
+		$num = mysql_affected_rows();
+		
+		if( $num == 1 ){
+			$_SESSION['user'] = $usuario;
+			header("Location: main.php");
+			exit(); 
+		}
+ 	}elseif ( $error ) {
 		session_destroy();
-		header("Location: register.php");
-		echo "alert('no se pudo registrar al usuario')";
+		header("Location: form.php");
 	}
+/*
 
+ 	header("Location: register.php?$var_Error");*/
 ?>
